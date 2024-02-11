3a51540fa6d7b2240b577b3337b1682467401387
jihaein <jihae.in@mail.mcgill.ca>
2024-02-10 23:28:19 -0500

    updated server.py

--- REACT-FLASK/flask-server/server.py
+++ REACT-FLASK/flask-server/server.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_restful import Api, Resource
 from penis import hi
 
 app = Flask(__name__)
+api = Api(app)
 
 # Members API Route
 @app.route("/members")
@@ -9,5 +11,20 @@ def members():
     hi()
     return {"members": ["Member1", "Member2", "Member3"]}
 
+names = {"tim" : {"age" : 19, "gender" : "male"}, 
+    "bill" : {"age" : 70, "gender" : "male"}}
+
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+    def post(self):
+        return {"data" : "Posted"}
+
+# User should input name after helloworld
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
