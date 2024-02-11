a9ceb5839ef709725b99ac8f68efb1cd870612cb
Skylar-gu <157906407+Skylar-gu@users.noreply.github.com>
2024-02-11 11:05:24 -0500

    Update emotionClassifier.py

--- emotionClassifier.py
+++ emotionClassifier.py
@@ -21,10 +21,29 @@ grief = emotion_outputs[14]
 fear = emotion_outputs[19]
 optimism = emotion_outputs[20]
 
-# example of getting the score
-sadness_score = sadness["score"]
-sadness_score = round(score, 2)
 
-print(model_outputs[0])
-print(sadness_score)
+def sadness_score():
+  return round(sadness["score"], 2)
+
+def neutral_score():
+  return round(neutral["score"], 2)
+
+def joy_score():
+  return round(joy["score"], 2)
+
+def anger_score():
+  return round(anger["score"], 2)
+
+def grief_score():
+  return round(grief["score"], 2)
+
+def fear_score():
+  return round(fear["score"], 2)
+
+def optimism_score():
+  return round(optimism["score"], 2)
+
+
+# print(model_outputs[0])
+# print(sadness_score)
 
